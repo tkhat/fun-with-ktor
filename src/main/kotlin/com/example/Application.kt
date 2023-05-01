e3741fbfcb95ae6d43bfef3a11bdea655c114484
tkhat <temurkhatiashvili@gmail.com>
2023-05-02 00:10:26 +0400

    hello ktor

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,15 @@
+package com.example
+
+import com.example.plugins.configureRouting
+import com.example.plugins.configureSerialization
+import io.ktor.server.application.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+
+}
